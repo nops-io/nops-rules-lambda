01e8884eab0825df0eb0a91009f03b2290ff43ad
Le Kien Truc <truc@nops.io>
2023-03-02 10:30:27 +0700

    NP-6170 Support start stop autoscaling group

---
+++ scheduler/src/tests/test_autoscaling_group.py
@@ -0,0 +1,86 @@
+import boto3
+from main import start_autoscaling_group
+from main import stop_autoscaling_group
+from moto import mock_autoscaling
+from moto import mock_eks
+
+EXAMPLE_AMI_ID = "ami-12c6146b"
+
+
+@mock_eks
+@mock_autoscaling
+def test_autoscaling_start():
+    conn = boto3.client("autoscaling", region_name="us-east-1")
+
+    conn.create_launch_configuration(
+        LaunchConfigurationName="TestLC",
+        ImageId=EXAMPLE_AMI_ID,
+        InstanceType="t2.medium",
+    )
+
+    conn.create_auto_scaling_group(
+        AutoScalingGroupName="TestGroup1",
+        MinSize=1,
+        DesiredCapacity=6,
+        MaxSize=10,
+        LaunchConfigurationName="TestLC",
+        AvailabilityZones=["us-east-1e"],
+        Tags=[
+            {
+                "ResourceId": "arn:TestGroup1",
+                "ResourceType": "auto-scaling-group",
+                "PropagateAtLaunch": True,
+                "Key": "TestTagKey1",
+                "Value": "TestTagValue1",
+            }
+        ],
+    )
+    resource = {
+        "resource_id": "TestGroup1",
+        "region": "us-east-1",
+        "resource_name": "TestGroup1",
+    }
+    action_details = {}
+    stop_autoscaling_group(resource, action_details)
+    details = conn.describe_auto_scaling_groups(AutoScalingGroupNames=["TestGroup1"])
+    assert details["AutoScalingGroups"][0]["DesiredCapacity"] == 0
+
+
+@mock_eks
+@mock_autoscaling
+def test_autoscaling_stop():
+    conn = boto3.client("autoscaling", region_name="us-east-1")
+
+    conn.create_launch_configuration(
+        LaunchConfigurationName="TestLC",
+        ImageId=EXAMPLE_AMI_ID,
+        InstanceType="t2.medium",
+    )
+
+    conn.create_auto_scaling_group(
+        AutoScalingGroupName="TestGroup1",
+        MinSize=1,
+        DesiredCapacity=6,
+        MaxSize=10,
+        LaunchConfigurationName="TestLC",
+        AvailabilityZones=["us-east-1e"],
+        Tags=[
+            {
+                "ResourceId": "arn:TestGroup1",
+                "ResourceType": "auto-scaling-group",
+                "PropagateAtLaunch": True,
+                "Key": "TestTagKey1",
+                "Value": "TestTagValue1",
+            }
+        ],
+    )
+    resource = {
+        "resource_id": "TestGroup1",
+        "region": "us-east-1",
+        "resource_name": "TestGroup1",
+        "resource_details": {"MinSize": 1, "MaxSize": 1, "DesiredCapacity": 1},
+    }
+    action_details = {}
+    start_autoscaling_group(resource, action_details)
+    details = conn.describe_auto_scaling_groups(AutoScalingGroupNames=["TestGroup1"])
+    assert details["AutoScalingGroups"][0]["DesiredCapacity"] == 1
