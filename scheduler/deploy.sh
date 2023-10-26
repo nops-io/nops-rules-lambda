520b57607d2c209623440aa43017261d56d2d5ab
Serhii Horodniuk <serhii@nops.io>
2023-10-26 14:10:55 +0300

    new stackset templates

--- scheduler/deploy.sh
+++ scheduler/deploy.sh
@@ -10,10 +10,12 @@ cd ..
 if [ "$ENV" == "prod" ];then
     aws s3 cp main-latest.zip s3://nops-rules-lambda-sources/scheduler/main-latest.zip --acl public-read
     aws s3 cp cloudformation_templates/scheduler.yml s3://nops-rules-lambda-sources/scheduler/scheduler.yml --acl public-read
+    aws s3 cp cloudformation_templates/scheduler-stackset.yml s3://nops-rules-lambda-sources/scheduler/scheduler-stackset.yml --acl public-read
     aws s3 cp cloudformation_templates/encrypted_ebs_kms_policy.yml s3://nops-rules-lambda-sources/scheduler/encrypted_ebs_kms_policy.yml --acl public-read
 else
     aws s3 cp main-latest.zip s3://nops-rules-lambda-sources/scheduler/main-latest-uat.zip --acl public-read
     aws s3 cp cloudformation_templates/scheduler.yml s3://nops-rules-lambda-sources/scheduler/scheduler-uat.yml --acl public-read
+    aws s3 cp cloudformation_templates/scheduler-stackset.yml s3://nops-rules-lambda-sources/scheduler/scheduler-stackset-uat.yml --acl public-read
     aws s3 cp cloudformation_templates/encrypted_ebs_kms_policy.yml s3://nops-rules-lambda-sources/scheduler/encrypted_ebs_kms_policy-uat.yml --acl public-read
 fi
 
@@ -27,7 +29,9 @@ cd ..
 if [ "$ENV" == "prod" ];then
     aws s3 cp essential-latest.zip s3://nops-rules-lambda-sources/essential/essential-latest.zip --acl public-read
     aws s3 cp cloudformation_templates/essential.yml s3://nops-rules-lambda-sources/essential/essential.yml --acl public-read
+    aws s3 cp cloudformation_templates/essential-stackset.yml s3://nops-rules-lambda-sources/essential/essential-stackset.yml --acl public-read
 else
     aws s3 cp essential-latest.zip s3://nops-rules-lambda-sources/essential/essential-latest-uat.zip --acl public-read
     aws s3 cp cloudformation_templates/essential.yml s3://nops-rules-lambda-sources/essential/essential-uat.yml --acl public-read
+    aws s3 cp cloudformation_templates/essential-stackset.yml s3://nops-rules-lambda-sources/essential/essential-stackset-uat.yml --acl public-read
 fi
